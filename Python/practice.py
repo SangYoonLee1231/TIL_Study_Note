fc27d6bd785b6e1d554417b8453b5e56bece0d56
SYL (SangYoonLee) <prestro1231@hufs.ac.kr>
2022-02-15 19:04:59 +0900

    Update All

--- Python/practice.py
+++ Python/practice.py
@@ -1,31 +1,6 @@
-n, r, c = list(map(int, input().split()))
+a, b = 3, (5, 6, 7, 4, 6)
 
-arr = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+a, (b1, b2, b3, b4, b5) = 3, (5, 6, 7, 4, 6)
 
-dxs = [-1, 1, 0, 0]
-dys = [0, 0 ,-1, 1]
-
-continue_move = True
-
-def in_range(x, y):
-    return x >= 0 and x <= n-1 and y >= 0 and y <= n-1
-
-def move(x, y):
-    global continue_move
-
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if arr[x][y] < arr[nx][ny] and in_range(nx, ny):
-            print(arr[x][y], end=" ")
-            return nx, ny
-    print(arr[x][y], end=" ")
-    continue_move = False
-    return x, y
-
-
-x, y = r-1, c-1
-while continue_move:
-    x, y = move(x, y)
+b1, b2, b3, b4, b5 = b
+print(b5)
