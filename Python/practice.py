89dd7db574e1859e2dbb12bd6e0f271486225ad2
SangYoonLee <sylagape1231@gmail.com>
2022-03-04 22:23:27 +0900

    Update All
    ※ JavaScript 기본 2 - 변수와 자료형, 상호작용, 형 변환
     ⊙ 오타 및 표현 수정

--- Python/practice.py
+++ Python/practice.py
@@ -1,16 +1,11 @@
-n = int(input())
 
-isPrime = True
-i = 2
 
-while i*i < n:
-    if n % i == 0:
-        isPrime = False
-        break
-    else:
-        i += 1
 
-if isPrime:
-    print(f"{n} is a prime number")
-else:
-    print(f"{n} is not a prime number")
+
+
+
+
+
+
+
+
