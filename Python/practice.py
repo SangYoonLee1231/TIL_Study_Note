e9f60f6bceb8a35fec170fda6fda8410a685b1c5
SangYoonLee <sylagape1231@gmail.com>
2022-08-13 00:08:24 +0900

    Update All
    2022-08-12 TIL CodeSoom
    # W2D5 - TIL 정리

--- Python/practice.py
+++ Python/practice.py
@@ -1,22 +1,51 @@
-k, n = tuple(map(int, input().split()))
-answer = []
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+
+snake = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [False] * (m)
+    for _ in range(n)
+]
+
+q = deque()
 
 
-def print_answer():
-    for elem in answer:
-        print(elem, end=" ")
-    print()
+def can_move(x, y):
+    if x < 0 or y < 0 or x >= n or y >= m:
+        return False
 
-
-def choose(curr_num, k_num):
-    if n > curr_num:
-        print_answer()
-        return
+    if not snake[x][y]:
+        return False
     
-    for i in range(1, k+1):
-        answer.append(i)
-        choose(curr_num + 1, i)
-        answer.pop()
+    return True
 
 
-choose(1, 1)
+def bfs():
+    x, y = q.popleft()
+
+    while q:
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+        for dx, dy in zip(dxs, dys):
+            nx = x + dx
+            ny = y + dy
+
+            if can_move(nx, ny) and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+                if nx == (n-1) and ny == (m-1):
+                    return
+
+
+visited[0][0] = True
+q.append((0, 0))
+bfs()
+
+print(1 if visited[n-1][m-1] else 0)
+
